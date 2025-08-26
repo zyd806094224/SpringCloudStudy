03bc3a43bf8c98a69585fb8d6807e038d7c87261
zhaoyudong <zhaoyudong@staff.hexun.com>
2025-08-26 20:17:38 +0800

    log日志

--- services/service-order/src/main/java/com/example/serviceorder/feign/fallback/ProductFeignClientFallback.java
+++ services/service-order/src/main/java/com/example/serviceorder/feign/fallback/ProductFeignClientFallback.java
@@ -3,14 +3,16 @@ package com.example.serviceorder.feign.fallback;
 
 import com.example.model.product.Product;
 import com.example.serviceorder.feign.ProductFeignClient;
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.stereotype.Component;
 
 import java.math.BigDecimal;
+@Slf4j
 @Component
 public class ProductFeignClientFallback implements ProductFeignClient {
     @Override
     public Product getProductById(Long id) {
-        System.out.println("Fallback...");
+        log.info("Fallback...");
         Product product = new Product();
         product.setId(id);
         product.setPrice(new BigDecimal("0"));
